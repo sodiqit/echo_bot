41bbee01819bbe0d41d5eaa41395d4a3d389f80d
Daniil <daniil.petro3@gmail.com>
2023-01-12 19:07:43 +0400

    feat: init structure

---
+++ src/config.rs
@@ -0,0 +1,63 @@
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, env, fs, path::PathBuf};
+use validator::{Validate, ValidationError};
+
+pub struct ConfigBuilder {
+    file_path: Option<String>,
+    file_content: Option<String>,
+}
+
+#[derive(Serialize, Validate, Deserialize, Debug)]
+pub struct Config {
+    mode: String,
+}
+
+impl ConfigBuilder {
+    pub fn new() -> Self {
+        ConfigBuilder {
+            file_path: Some("config.yaml".to_string()),
+            file_content: None,
+        }
+    }
+
+    pub fn extract_path(mut self) -> Self {
+        let path = env::args().nth(1);
+
+        if path.is_some() {
+            self.file_path = Some(path.unwrap());
+        }
+
+        self
+    }
+
+    pub fn extract_config_body(mut self) -> Self {
+        let mut path = PathBuf::from(env::current_dir().expect("Cannot get current directory"));
+        path.push(self.file_path.as_ref().unwrap());
+
+        let content = fs::read_to_string(path).expect(&format!(
+            "Cannot read file with current path: {}",
+            self.file_path.as_ref().unwrap()
+        ));
+
+        self.file_content = Some(content);
+
+        self
+    }
+
+    pub fn build(self) -> Config {
+        let config: Config = serde_yaml::from_str(self.file_content.as_ref().unwrap())
+            .map_err(|e| format!("Parse config failed: {}", e))
+            .unwrap();
+
+        config
+    }
+
+    fn validate(&self, config: &Config) {
+        let iterable_headers: HashMap<String, String> =
+            serde_yaml::from_value(serde_yaml::to_value(config).unwrap()).unwrap();
+
+        for header in &iterable_headers {
+            println!("{:?}", header);
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+mod config;
+
+use config::ConfigBuilder;
+
+fn main() {
+    let config = ConfigBuilder::new()
+        .extract_path()
+        .extract_config_body()
+        .build();
+
+    println!("{:?}", config);
+}
