42bc7f65beca8dc319d762cecfc33c73e6b067e5
Daniil <daniil.petro3@gmail.com>
2023-01-25 14:47:30 +0400

    fix: throw error in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::process;
+
 use echo_bot::config::ConfigBuilder;
 use echo_bot::logger::{ConsoleLogger, Logger};
+use echo_bot::{BotError, ClientError};
 
 fn main() {
     let config = ConfigBuilder::new()
@@ -10,5 +13,24 @@ fn main() {
     let logger = ConsoleLogger::new(config.log_level.clone());
     logger.log_info("logger with config success build");
 
-    echo_bot::run_bot(config, &logger);
+    match echo_bot::run_bot(config, &logger) {
+        Err(BotError::Console(err)) => {
+            logger.log_error(format!("io error occurred: {}", err).as_str())
+        }
+        Err(BotError::Telegram(error)) => {
+            match error {
+                ClientError::Api(e) => {
+                    logger.log_error(format!("get error from api telegram: {:?}", e).as_str())
+                }
+                ClientError::Http(e) => {
+                    logger.log_error(format!("get error from http client: {}", e).as_str())
+                }
+                ClientError::Serialize(e) => {
+                    logger.log_error(format!("serialization error occurred : {}", e).as_str())
+                }
+            }
+            process::exit(1);
+        }
+        Ok(_) => {}
+    }
 }
